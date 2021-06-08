1b7a5516c78575826379c284d578acdf253aa9c9
Its-Ace <68051549+Its-Ace@users.noreply.github.com>
2021-06-07 23:12:35 -0700

    Joels

---
+++ Public/Basic Technologies/Plugin-Loading by Joel/SDT_PLUGINS/hello-world/Plugin2/MyPlugin2.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Nate McMaster.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using HelloWorld;
+
+namespace Plugin2
+{
+    internal class MyPlugin2 : IPlugin
+    {
+        public string GetName() => "plugin J2";
+
+    }
+}
